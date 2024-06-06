a92f5a913ba6f85e1532bbd3a4730bd5ff5e941f
Jed Staley <JNStaley1@gmail.com>
2024-06-05 21:51:56 -0400

    finished lab 28

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -4,6 +4,7 @@ import './Form.scss';
 
 const Form = (props) => {
   const [method, setMethod] = useState('GET');
+  const [url, setUrl] = useState('');
   const [jsonData, setJsonData] = useState('');
 
   const handleMethodClick = (e) => {
@@ -14,73 +15,71 @@ const Form = (props) => {
     setJsonData(e.target.value);
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const formData = {
       method: method,
-      url: 'https://pokeapi.co/api/v2/pokemon',
+      url: url,
       data: jsonData && (method === 'POST' || method === 'PUT') ? JSON.parse(jsonData) : null,
     };
     props.handleApiCall(formData);
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit}>
+      <label>
+        <span>URL: </span>
+        <input name="url" type="text" value={url} onChange={(e) => setUrl(e.target.value)} required />
+        <button type="submit">GO!</button>
+      </label>
+      <div className="methods">
+        <button
+          type="button"
+          id="GET"
+          onClick={handleMethodClick}
+          className={method === 'GET' ? 'active' : ''}
+        >
+          GET
+        </button>
+        <button
+          type="button"
+          id="POST"
+          onClick={handleMethodClick}
+          className={method === 'POST' ? 'active' : ''}
+        >
+          POST
+        </button>
+        <button
+          type="button"
+          id="PUT"
+          onClick={handleMethodClick}
+          className={method === 'PUT' ? 'active' : ''}
+        >
+          PUT
+        </button>
+        <button
+          type="button"
+          id="DELETE"
+          onClick={handleMethodClick}
+          className={method === 'DELETE' ? 'active' : ''}
+        >
+          DELETE
+        </button>
+      </div>
+      {(method === 'POST' || method === 'PUT') && (
         <label>
-          <span>URL: </span>
-          <input name='url' type='text' />
-          <button type="submit">GO!</button>
+          <span>JSON Data: </span>
+          <textarea
+            name="jsonData"
+            rows="10"
+            cols="50"
+            value={jsonData}
+            onChange={handleJsonChange}
+          />
         </label>
-        <div className="methods">
-          <button
-            type="button"
-            id="GET"
-            onClick={handleMethodClick}
-            className={method === 'GET' ? 'active' : ''}
-          >
-            GET
-          </button>
-          <button
-            type="button"
-            id="POST"
-            onClick={handleMethodClick}
-            className={method === 'POST' ? 'active' : ''}
-          >
-            POST
-          </button>
-          <button
-            type="button"
-            id="PUT"
-            onClick={handleMethodClick}
-            className={method === 'PUT' ? 'active' : ''}
-          >
-            PUT
-          </button>
-          <button
-            type="button"
-            id="DELETE"
-            onClick={handleMethodClick}
-            className={method === 'DELETE' ? 'active' : ''}
-          >
-            DELETE
-          </button>
-        </div>
-        {(method === 'POST' || method === 'PUT') && (
-          <label>
-            <span>JSON Data: </span>
-            <textarea
-              name='jsonData'
-              rows="10"
-              cols="50"
-              value={jsonData}
-              onChange={handleJsonChange}
-            />
-          </label>
-        )}
-      </form>
-    </>
+      )}
+    </form>
   );
-}
+};
 
 export default Form;
